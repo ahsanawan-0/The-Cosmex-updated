b3366de68fa9755104a42c2e752b1fa3aef16348
Ahsan Awan <ahsanawan0482@gmail.com>
2026-05-18 21:35:17 +0500

    fix erros

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,4 +1,9 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+@php
+/** @var \Illuminate\Support\Collection $products */
+/** @var \Illuminate\Support\Collection $categories */
+/** @var array $pages */
+@endphp
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     @foreach($pages as $page)
     <url>
